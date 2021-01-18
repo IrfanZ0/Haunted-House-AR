1a09c1ac34b1a278c2d076dc676b277f83a11b07
IrfanZ <iziaulla@gmail.com>
2021-01-18 07:03:07 -0800

    Part 3 Enemy AI
    Part 3 enemy AI

---
+++ Assets/Scripts/Enemies/Enemy AI/PatrolAction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PluggableAI/Actions/Patrol")]
+public class PatrolAction : Action
+{
+    float power;
+    public override void Act(StateController controller)
+    {
+            Patrol ( controller );
+         
+    }
+
+
+    private void Patrol(StateController controller)
+    {
+        controller.navMeshAgent.SetDestination(controller.wayPointList [ controller.nextWayPoint ].position);
+
+       
+
+        if (controller.navMeshAgent.remainingDistance < controller.navMeshAgent.stoppingDistance && !controller.navMeshAgent.pathPending)
+        {
+            controller.navMeshAgent.isStopped = true;
+            controller.animator.SetFloat ( "Speed" , 5.0f );
+            controller.navMeshAgent.autoBraking = true;
+            LancerAttack lancerAttack = controller.navMeshAgent.gameObject.GetComponent<LancerAttack>();
+            power = 20f;
+            lancerAttack.Attack ( power );
+            controller.navMeshAgent.ResetPath ( );
+            power = 0;
+            lancerAttack.Attack ( power );
+            controller.animator.SetFloat ( "Power" , power );
+            controller.navMeshAgent.isStopped = false;
+            controller.animator.SetFloat ( "Speed" , 15.0f );
+            controller.nextWayPoint = ( controller.nextWayPoint + 1 ) % controller.wayPointList.Count;
+            controller.navMeshAgent.SetDestination ( controller.wayPointList [ controller.nextWayPoint + 1 ].position );
+
+
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Enemies/Enemy AI/StateController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StateController : MonoBehaviour
+{
+    public State currentState;
+    public EnemyStats enemyStats;
+    public Transform eyes;
+    public State remainState;
+    [HideInInspector] public NavMeshAgent navMeshAgent;
+    [HideInInspector] public Animator animator;
+    [HideInInspector] public List<Transform> wayPointList;
+    [HideInInspector] public int nextWayPoint;
+    [HideInInspector] public Transform chaseTarget;
+    [HideInInspector] public float stateTimeElapsed;
+
+    private bool aiActive;
+
+
+    void Awake()
+    {
+        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator> ( );
+    }
+
+    public void SetupAI(bool aiActivationFromEnemyManager, List<Transform> wayPointsFromEnemyManager)
+    {
+        wayPointList = wayPointsFromEnemyManager;
+        aiActive = aiActivationFromEnemyManager;
+        if (aiActive)
+        {
+            navMeshAgent.enabled = true;
+        }
+        else
+        {
+            navMeshAgent.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (!aiActive)
+            return;
+
+        currentState.UpdateState(this);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (currentState != null && eyes != null)
+        {
+            Gizmos.color = currentState.sceneGizmoColor;
+            Gizmos.DrawWireSphere(eyes.position, enemyStats.lookSphereCastRadius);
+        }
+    }
+
+    public void TransitionToState(State nextState)
+    {
+        if (nextState != remainState)
+        {
+            currentState = nextState;
+            OnExitState();
+        }
+    }
+
+    public bool CheckIfCountDownElapsed(float duration)
+    {
+        stateTimeElapsed += Time.deltaTime;
+        return (stateTimeElapsed >= duration);
+    }
+
+    private void OnExitState()
+    {
+        stateTimeElapsed = 0;
+    }
+}
